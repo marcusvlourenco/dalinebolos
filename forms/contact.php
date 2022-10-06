2ecd5dc9fc969c9561deae30498987c323ed5b79
marcusvlourenco <marcusvvlourenco@gmail.com>
2022-10-06 01:11:00 -0300

    email

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,71 @@
 <?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$arquivo = "
+  <style type='text/css'>
+  body {
+  margin:0px;
+  font-family:Verdane;
+  font-size:12px;
+  color: #666666;
+  }
+  a{
+  color: #666666;
+  text-decoration: none;
+  }
+  a:hover {
+  color: #FF0000;
+  text-decoration: none;
+  }
+  </style>
+    <html>
+        <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
+            <tr>
+              <td>
+  <tr>
+                 <td width='500'>Name:$name</td>
+                </tr>
+                <tr>
+                  <td width='320'>E-mail:<b>$email</b></td>
+     </tr>
+      <tr>
+                  <td width='320'>Subject:<b>$subject</b></td>
+                </tr>
+     <tr>
+                  <td width='320'>Message:$message</td>
+                </tr>
+            </td>
+          </tr>
+        </table>
+    </html>
+  ";
+//enviar
+
+  // emails para quem será enviado o formulário
+  $emailenviar = "dalinebolos@gmail.com";
+  $destino = $emailenviar;
+  $assunto = "Contato pelo Site";
+
+  // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $headers .= 'From: $name <$email>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
+
+  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
+  if($enviaremail){
+  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+  } else {
+  $mgm = "ERRO AO ENVIAR E-MAIL!";
+  echo "";
+  }
+
+
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -6,7 +73,8 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
+  // Replace contact@example.com with your real receiving email address/*
+  /*
   $receiving_email_address = 'dalinebolos@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -22,20 +90,20 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-
+*/
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
- 
+ /*
   $contact->smtp = array(
     'host' => 'dalinebolos@gmail.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  
-
+  */
+/*
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send();*/
 ?>
