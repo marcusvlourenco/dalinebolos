f2b78284866212286805c4d844ce2cbcafd2d662
marcusvlourenco <marcusvvlourenco@gmail.com>
2022-12-01 21:49:39 -0300

    c

--- formulario/contato.php
+++ formulario/contato.php
@@ -1,90 +1,78 @@
 <?php 
+  
+    //Convertendo caracteres especiais para a realidade HTML
+    $name = htmlspecialchars_decode(htmlspecialchars($_POST['name'], ENT_QUOTES,'UTF-8'));
+    $email = htmlspecialchars_decode(htmlspecialchars($_POST['email'], ENT_QUOTES,'UTF-8'));
+    $phone = htmlspecialchars_decode(htmlspecialchars($_POST['phone'], ENT_QUOTES,'UTF-8'));
+    $subject = htmlspecialchars_decode(htmlspecialchars($_POST['subject'], ENT_QUOTES,'UTF-8'));
+    $message = htmlspecialchars_decode(htmlspecialchars($_POST['message'], ENT_QUOTES,'UTF-8'));
 
-function isEmailValido($email){
-    $conta = "/[a-zA-Z0-9\._-]+@";
-    $domino = "[a-zA-Z0-9\._-]+.";
-    $extensao = "([a-zA-Z]{2,4})$/";
-    $pattern = $conta.$domino.$extensao; 
-    if (preg_match($pattern, $email))
-        return false;
-    else
-        return true;
-}
-
-if ( !isset( $_POST ) || empty( $_POST ) ) {
-    echo "<script>alert('Envio inválido!');window.history.back();</script>";
-}else{
-    $captcha = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : null;    
-    if(!is_null($captcha)){
-        $res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Ldq4EcjAAAAAMbl6C9qHkfDPiGAWMRX3e1C896H&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']));
-        if($res->success === true){            
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $tamanho = strlen($name);
-            if($tamanho < 3){        
-                echo "<script>alert('Nome inválido!');window.history.back();</script>";
-            }
-            if(isEmailValido($email) && filter_var($email, FILTER_VALIDATE_EMAIL)){                
-                echo "<script>alert('E-mail inválido!');window.history.back();</script>";
-            }
-            $subject = $_POST['subject'];
-            $phone = $_POST['phone'];
-            $tamanhop = strlen($phone);
-            if($tamanhop < 14){        
-                echo "<script>alert('Telefone inválido!');window.history.back();</script>";
-            }
-            $message = $_POST['message'];
-            $to = "dalinebolos@gmail.com";
-            $message = "
-            <html>
-                <head>
-                    <title>HTML email</title>
-                </head>
-                <body>
-                    <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#ebb2ed'>
-                        <tr>
-                            <th colspan='2'>Contato</th>
-                        </tr>
-                        <tr>
-                            <td width='500'>Nome:$name</td>
-                        
-                            <td width='320'>E-mail:<b>$email</b></td>
-                        </tr>
-                        <tr>
-                            <td width='320'>Assunto:<b>$subject</b></td>
-                        
-                            <td width='320'>Telefone:<b>$phone</b></td>
-                        </tr>
-                        <tr>
-                            <th width='320'  colspan='2'  rowspan='2'>Mensagem: $message</th>
-                        </tr>
-                            
-                    </table>
-                </body>
-            </html>
-            ";
-            // Always set content-type when sending HTML email
-            $headers = "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            // More headers
-            $headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
-            $headers .= 'Cc: '.$email . "\r\n";
-            $enviaremail = mail($to,$subject,$message,$headers);
-            if ($enviaremail) {
-                echo "<script>alert('E-MAIL ENVIADO COM SUCESSO!');location.href='../index.html#Section-Contato';</script>";
-            }
-            else {                
-                echo "<script>alert('ERRO AO ENVIAR E-MAIL.');</script>";
-                echo "<script>alert('<b>Detalhes do erro:</b>');</script>";
-                echo "<script>alert($mail->ErrorInfo);</script>";
-                echo "<script>window.history.back();</script>";
-            }
-        }else{
-            echo "<script>alert('Erro ao validar o captcha!!!');window.history.back();location.reload(true);</script>";
-        }
-    }else{
-        echo "<script>alert('Captcha não preenchido!');window.history.back();</script>";
+    function isEmailValido($email){
+        $conta = "/[a-zA-Z0-9\._-]+@";
+        $domino = "[a-zA-Z0-9\._-]+.";
+        $extensao = "([a-zA-Z]{2,4})$/";
+        $pattern = $conta.$domino.$extensao; 
+        return preg_match($pattern, $email);
     }
-}
-    
+
+    if((empty($name) == true) ||  ((strlen($name) < 3) == true)){    
+        echo "<script>alert('Nome precisa ser preenchido corretamente...');window.history.back();</script>";
+    } elseif(((strlen($email)<8) == true) || (strstr($email,'@', true) == false) || (strstr($email,'.',true) == false) || ((isEmailValido($email))==false) || (filter_var( $email, FILTER_VALIDATE_EMAIL) == false)){
+        echo "<script>alert('Email precisa ser preenchido corretamente...');window.history.back();</script>";
+    } elseif((empty($subject) == true) || ((strlen($subject) < 3) == true)){
+        echo "<script>alert('Assunto precisa ser preenchido...');window.history.back();</script>";
+    } elseif((empty($phone) == true) || ((strlen($phone) < 14) == true)){
+        echo "<script>alert('Telefone precisa ser preenchido...');window.history.back();</script>";
+    } elseif((empty($message) == true) || ((strlen($message) < 2) == true)){
+        echo "<script>alert('Pedido precisa ser preenchido...');window.history.back();</script>";
+    } elseif ( !isset( $_POST ) || empty( $_POST ) ) {
+        echo "<script>alert('Nada Foi Postado!');window.history.back();</script>";
+    } else {
+        $to = "dalinebolos@gmail.com";
+        $mensagem = "
+        <html>
+            <head>
+                <title>HTML email</title>
+            </head>
+            <body>
+                <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#ebb2ed'>
+                    <tr>
+                        <th colspan='2'>Contato</th>
+                    </tr>
+                    <tr>
+                        <td width='500'>Nome:$name</td>
+                    
+                        <td width='320'>E-mail:<b>$email</b></td>
+                    </tr>
+                    <tr>
+                        <td width='320'>Assunto:<b>$subject</b></td>
+                    
+                        <td width='320'>Telefone:<b>$phone</b></td>
+                    </tr>
+                    <tr>
+                        <th width='320'  colspan='2'  rowspan='2'>Mensagem: $message</th>
+                    </tr>
+                        
+                </table>
+            </body>
+        </html>
+        ";
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        // More headers
+        $headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
+        $headers .= 'Cc: '.$email . "\r\n";
+        $enviaremail = mail($to,$subject,$mensagem,$headers);
+        if ($enviaremail) {
+            echo "<script>alert('MENSAGEM ENVIADA COM SUCESSO!');</script>";
+            echo "<script>alert('AGRADECEMOS O SEU CONTATO E EM BREVE RETORNAREMOS!');location.href='../index.html';</script>";
+        }
+        else {                
+            echo "<script>alert('ERRO AO ENVIAR A MANSAGEM DE CONTATO.');</script>";
+            echo "<script>alert('<b>Detalhes do erro:</b>');</script>";
+            echo "<script>alert($mail->ErrorInfo);</script>";
+            echo "<script>window.history.back();</script>";
+        }  
+    }
 ?>
--- formulario/orcamento.php
+++ formulario/orcamento.php
@@ -1,94 +1,81 @@
 <?php 
+  
+  //Convertendo caracteres especiais para a realidade HTML
+  $name = htmlspecialchars_decode(htmlspecialchars($_POST['name'], ENT_QUOTES,'UTF-8'));
+  $email = htmlspecialchars_decode(htmlspecialchars($_POST['email'], ENT_QUOTES,'UTF-8'));
+  $phone = htmlspecialchars_decode(htmlspecialchars($_POST['phone'], ENT_QUOTES,'UTF-8'));
+  $date = htmlspecialchars_decode(htmlspecialchars($_POST['date'], ENT_QUOTES,'UTF-8'));
+  $time = htmlspecialchars_decode(htmlspecialchars($_POST['time'], ENT_QUOTES,'UTF-8'));
+  $local = htmlspecialchars_decode(htmlspecialchars($_POST['local'], ENT_QUOTES,'UTF-8'));
+  $message = htmlspecialchars_decode(htmlspecialchars($_POST['message'], ENT_QUOTES,'UTF-8'));
 
-function isEmailValido($email){
+  function isEmailValido($email){
     $conta = "/[a-zA-Z0-9\._-]+@";
     $domino = "[a-zA-Z0-9\._-]+.";
     $extensao = "([a-zA-Z]{2,4})$/";
     $pattern = $conta.$domino.$extensao; 
-    if (preg_match($pattern, $email))
-        return false;
-    else
-        return true;
-}
-
-if ( !isset( $_POST ) || empty( $_POST ) ) {
-    echo "<script>alert('Envio inválido!');window.history.back();</script>";
-}else{
-    $captcha = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : null;    
-    if(!is_null($captcha)){
-        $res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Ldq4EcjAAAAAMbl6C9qHkfDPiGAWMRX3e1C896H&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']));
-        if($res->success === true){   
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $tamanho = strlen($name);
-            if($tamanho < 3){        
-                echo "<script>alert('Nome inválido!');window.history.back();</script>";
-            }
-            if(isEmailValido($email) && filter_var($email, FILTER_VALIDATE_EMAIL)){                
-                echo "<script>alert('E-mail inválido!');window.history.back();</script>";
-            }             
-            $phone = $_POST['phone'];
-            $tamanhop = strlen($phone);
-            if($tamanhop < 14){        
-                echo "<script>alert('Telefone inválido!');window.history.back();</script>";
-            }
-            $date = $_POST['date'];           
-            $time = $_POST['time'];  
-            $local = $_POST['local'];
-            $message = $_POST['message'];
-            $message = "
-              <html> 
-                <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#ebb2ed'>
-                  <tr>
-                      <th colspan='2'>Orçamento</th>
-                  </tr>
-                  <tr>
-                      <td width='500'>Nome:$name</td>
-                      <td width='320'>E-mail:<b>$email</b></td>
-                  </tr>
-                  <tr>
-                      <td width='320'>Telefone:$phone</td>
-                      <td width='500'>Local Entrega:<b>$local</b></td>
-                  
-                  </tr>
-                  <tr>
-                      <td width='320'>Data:<b>$date</b></td>
-                  
-                      <td width='320'>Horário:<b>$time</b></td>
-                  </tr>
-                  <tr>
-                      <th width='320'  colspan='2'  rowspan='2'>Pedido: $message</th>
-                  </tr>
-                    
-                </table>
-                
-                </html>
-              ";
-            // emails para quem será enviado o formulário
-            $subject = "Orçamento pelo Site";
-            $to = "dalinebolos@gmail.com";
-              // Always set content-type when sending HTML email
-            $headers = "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            // More headers
-            $headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
-            $headers .= 'Cc: '.$email . "\r\n";          
-            $enviaremail = mail($to,$subject,$message,$headers);
-            if ($enviaremail) {
-                echo "<script>alert('E-MAIL ENVIADO COM SUCESSO!');location.href='../index.html#Section-Orcamento';</script>";
-            }
-            else {                
-                echo "<script>alert('ERRO AO ENVIAR E-MAIL.');</script>";
-                echo "<script>alert('<b>Detalhes do erro:</b>');</script>";
-                echo "<script>alert($mail->ErrorInfo);</script>";
-                echo "<script>window.history.back();</script>";
-            }
-        }else{
-            echo "<script>alert('Erro ao validar o captcha!!!');window.history.back();location.reload(true);</script>";
-        }
-    }else{
-        echo "<script>alert('Captcha não preenchido!');window.history.back();</script>";
+    return preg_match($pattern, $email);
+  }
+  
+  if((empty($name) == true) || (strlen($name) < 3) == true){    
+      echo "<script>alert('Nome precisa ser preenchido corretamente...');window.history.back();</script>";
+  } elseif(((strlen($email)<8 ) == true) || (strstr($email,'@', true) == false) || (strstr($email,'.',true) == false) || ((isEmailValido($email))==false) || (filter_var( $email, FILTER_VALIDATE_EMAIL) == false)){
+      echo "<script>alert('Email precisa ser preenchido corretamente...');window.history.back();</script>";
+  } elseif((empty($date) == true) || (strlen($date) < 10) == true){
+    echo "<script>alert('Data precisa ser preenchida...');window.history.back();</script>";
+  } elseif((empty($time) == true)  || (strlen($time) < 5) == true){
+    echo "<script>alert('Horário precisa ser preenchido...');window.history.back();</script>";
+  } elseif((empty($local) == true)  || (strlen($local) < 8) == true){
+    echo "<script>alert('O Endereço precisa ser preenchido...');window.history.back();</script>";
+  } elseif((empty($phone) == true)  || (strlen($phone) < 14) == true){
+      echo "<script>alert('Telefone precisa ser preenchido...');window.history.back();</script>";
+  } elseif((empty($message) == true)  || (strlen($message) < 2) == true){
+      echo "<script>alert('Pedido precisa ser preenchido...');window.history.back();</script>";
+  } elseif ( !isset( $_POST ) || empty( $_POST ) ) {
+    echo "<script>alert('Nada Foi Postado!');window.history.back();</script>";
+  } else {
+    $mensagem = "
+      <html> 
+        <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#ebb2ed'>
+          <tr>
+              <th colspan='2'>Orçamento</th>
+          </tr>
+          <tr>
+              <td width='500'>Nome:$name</td>
+              <td width='320'>E-mail:<b>$email</b></td>
+          </tr>
+          <tr>
+              <td width='500'>Local Entrega:<b>$local</b></td>
+              <td width='320'>Telefone:$phone</td>          
+          </tr>
+          <tr>
+              <td width='500'>Data:<b>$date</b></td>          
+              <td width='320'>Horário:<b>$time</b></td>
+          </tr>
+          <tr>
+              <th width='820'  colspan='2'  rowspan='2'>Pedido: $message</th>
+          </tr>
+            
+        </table>
+        
+        </html>
+      ";
+    $subject = "Orçamento pelo Site";
+    $to = "dalinebolos@gmail.com";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: '.$name.' <'.$email.'>' . "\r\n";
+    $headers .= 'Cc: '.$email . "\r\n";          
+    $enviaremail = mail($to,$subject,$mensagem,$headers);
+    if ($enviaremail) {
+      echo "<script>alert('ORÇAMENTO ENVIADO COM SUCESSO!');</script>";
+      echo "<script>alert('AGRADECEMOS A SUA PREFERÊNCIA E EM BREVE ENTRAREMOS EM CONTATO!');location.href='../index.html';</script>";
     }
-}
-    
+    else {                
+        echo "<script>alert('ERRO AO ENVIAR PEDIDO DE ORÇAMENTO.');</script>";
+        echo "<script>alert('<b>Detalhes do erro:</b>');</script>";
+        echo "<script>alert($mail->ErrorInfo);</script>";
+        echo "<script>window.history.back();</script>";
+    } 
+  }
 ?>
